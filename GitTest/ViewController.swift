73a2d6b2024dce4637f248387f613575421ca82e
dsiegler19 <dsiegler19@us.edu>
2018-06-18 11:09:04 -0400

    Added a button that you can click

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -11,13 +11,21 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        print("button was clicked")
+        
+    }
+    
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 
